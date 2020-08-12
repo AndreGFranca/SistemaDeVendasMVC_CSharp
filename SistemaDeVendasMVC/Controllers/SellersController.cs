94ebd0974a4d7dc225da6f7fc07a69b9b6bf5bce
andregf1 ADARK <andre.franca2@etec.sp.gov.br>
2020-08-12 10:38:22 -0300

    Função editar Implementada a classe Seller

--- SistemaDeVendasMVC/Controllers/SellersController.cs
+++ SistemaDeVendasMVC/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using SistemaDeVendasMVC.Models;
 using SistemaDeVendasMVC.Models.ViewModels;
 using SistemaDeVendasMVC.Services;
+using SistemaDeVendasMVC.Services.Exceptions;
 
 namespace SistemaDeVendasMVC.Controllers
 {
@@ -64,6 +65,40 @@ namespace SistemaDeVendasMVC.Controllers
                 return NotFound();
             }
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id) {
+            if (id == null) {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null) {
+                return NotFound();
+            }
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Edit(int id, Seller seller) {
+            if (id != seller.Id) {
+                return BadRequest();
+            }
+            try {
+
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException) {
+                return NotFound();
+            }
+            catch (DbConcurrencyException) {
+                return BadRequest();
+            }
+
         }
     }
 }
--- SistemaDeVendasMVC/Services/SellerService.cs
+++ SistemaDeVendasMVC/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using SistemaDeVendasMVC.Data;
 using SistemaDeVendasMVC.Models;
 using Microsoft.EntityFrameworkCore;
+using SistemaDeVendasMVC.Services.Exceptions;
 
 namespace SistemaDeVendasMVC.Services {
     public class SellerService {
@@ -29,6 +30,19 @@ namespace SistemaDeVendasMVC.Services {
             var obj = _context.Sellers.Find(id);
             _context.Sellers.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj) {
+            if (!_context.Sellers.Any(x=> x.Id == obj.Id)) {
+                throw new NotFoundException("Id not found");
+            }
+            try {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e) {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
