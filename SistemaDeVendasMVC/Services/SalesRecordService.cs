55d915ea032958433379731b7daace8b96971ac8
andregf1 ADARK <andre.franca2@etec.sp.gov.br>
2020-08-12 14:44:38 -0300

    Implementando a busca Simples

---
+++ SistemaDeVendasMVC/Services/SalesRecordService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeVendasMVC.Data;
+using SistemaDeVendasMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaDeVendasMVC.Services {
+    public class SalesRecordService {
+        private readonly SistemaDeVendasMVCContext _context;
+
+        public SalesRecordService(SistemaDeVendasMVCContext context) {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate,DateTime? maxDate) {
+            var result = from obj in _context.SalesRecords select obj;
+            if (minDate.HasValue) {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue) {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+    }
+}
