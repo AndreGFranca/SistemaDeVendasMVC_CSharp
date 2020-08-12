2e5d5f23a0603b3f95cbf64bfda30e0e04488df1
andregf1 ADARK <andre.franca2@etec.sp.gov.br>
2020-08-12 08:09:58 -0300

    Adicionada uma foreign key a classe seller, feita uma nova migration e um novo update do banco.

--- SistemaDeVendasMVC/Models/Seller.cs
+++ SistemaDeVendasMVC/Models/Seller.cs
@@ -12,6 +12,8 @@ namespace SistemaDeVendasMVC.Models {
 
         public Department Department { get; set; }
 
+        public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller() {
--- SistemaDeVendasMVC/Services/SellerService.cs
+++ SistemaDeVendasMVC/Services/SellerService.cs
@@ -17,6 +17,7 @@ namespace SistemaDeVendasMVC.Services {
         }
 
         public void Insert(Seller obj) {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
