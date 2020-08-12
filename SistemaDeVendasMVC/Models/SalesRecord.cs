906eda49bec0b800441b4b202348c62c6bac9e35
andregf1 ADARK <andre.franca2@etec.sp.gov.br>
2020-08-12 15:58:27 -0300

    Implementando Grouping search

--- SistemaDeVendasMVC/Models/SalesRecord.cs
+++ SistemaDeVendasMVC/Models/SalesRecord.cs
@@ -1,11 +1,14 @@
 ﻿using SistemaDeVendasMVC.Models.Enums;
 using System;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace SistemaDeVendasMVC.Models {
     public class SalesRecord {
         public int Id { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
 
